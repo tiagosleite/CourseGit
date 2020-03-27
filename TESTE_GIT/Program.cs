51889ba12e9f753f47ec1248f1fdb76f45d5e8b5
Tiago Silva <tiago.silvaleite@gmail.com>
2020-03-27 11:38:07 -0300

    Projeto Iniciado.

---
+++ TESTE_GIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TESTE_GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
